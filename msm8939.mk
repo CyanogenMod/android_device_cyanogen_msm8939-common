d7deab24dc4643e12759f578a6e26fb6a5475303
Ethan Chen <intervigil@gmail.com>
2015-05-04 18:47:02 -0700

    msm8939: Move GPS HAL to device
    Change-Id: Id0075b4ec6cf7b4bbd30dbc255b1ad4c4be4728e

--- msm8939.mk
+++ msm8939.mk
@@ -105,17 +105,6 @@ PRODUCT_PACKAGES += \
     libqcomfm_jni \
     qcom.fmradio
 
-# GPS
-PRODUCT_PACKAGES += \
-    gps.msm8916
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
-
 # IRQ balance
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/etc/msm_irqbalance.conf
