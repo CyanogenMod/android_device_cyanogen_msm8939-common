a81f3287011fbfc039783dc8b72a3fd118faca14
Ethan Chen <intervigil@gmail.com>
2015-07-23 10:57:46 -0700

    msm8939: All targets have signed venus firmware
    Change-Id: I5e02d687a87b8aa614ae302326bd58209550c122

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -145,6 +145,9 @@ BOARD_SEPOLICY_UNION += \
     system_server.te \
     system.te
 
+# Video
+TARGET_HAVE_SIGNED_VENUS_FW := true
+
 # Vold
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
