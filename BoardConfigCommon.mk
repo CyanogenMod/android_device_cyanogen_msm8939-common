b3d8898905247411940cf8640e89c4e79e7aa71a
Ethan Chen <intervigil@gmail.com>
2015-11-04 10:54:05 -0800

    msm8939: Don't use custom mkbootimg
    Change-Id: Ic3aac75ac9a2dfdfa433ea9964109aa46d8323ae

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,7 +45,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := $(VENDOR_PATH)/mkbootimg.mk
+BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_PAGESIZE := 2048
